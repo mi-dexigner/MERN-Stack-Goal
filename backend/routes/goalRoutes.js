5f182987af4ec710dcd6057db64d6b8cad6fbe79
Muhammad Idrees <midexigner@gmail.com>
2022-03-23 19:42:44 +0500

    create a basic controller connect with router GET,POST,PUT,DELETE

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,21 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const {getGoals,setGoal,updateGoal,deleteGoal} = require("../controllers/goalControllers");
 
+router.get('/',getGoals);
 
-router.get('/',(req,res)=>{
-    res.status(200).json({message:"Get goal"})
-})
+router.post('/',setGoal);
 
-router.post('/',(req,res)=>{
-    res.status(200).json({message:"Set goals"})
-})
+router.put('/:id',updateGoal);
 
-router.put('/:id',(req,res)=>{
-    res.status(200).json({message:`Update goal ${req.params.id}`})
-})
-
-router.delete('/:id',(req,res)=>{
-    res.status(200).json({message:`Delete goal ${req.params.id}`})
-})
+router.delete('/:id',deleteGoal);
 
 module.exports = router;
