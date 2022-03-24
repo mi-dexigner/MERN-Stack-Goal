7583920a3951a40621462b8d5d137ac3da67145a
Muhammad Idrees <midexigner@gmail.com>
2022-03-24 18:41:42 +0500

    error and Exception Handler

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const dotenv = require('dotenv').config();
+const {errorHandler} = require('./middleware/errorMiddleware');
 const port =  process.env.PORT || 5000;
 
 const app = express();
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
 app.use('/api/goals',require('./routes/goalRoutes'));
+
+app.use(errorHandler);
+
+app.listen(port,()=>console.log(`Server started on port ${port}`))
+// npm run server
 
 // Change the 404 message modifing the middleware
 app.use(function(req, res, next) {
     res.status(404).send("Sorry, that route doesn't exist. Have a nice day :)");
 });
-
-app.listen(port,()=>console.log(`Server started on port ${port}`))
-// npm run server
-
