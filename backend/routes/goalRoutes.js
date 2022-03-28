a20e5672c7d8689b679abf3ce425febed4b64065
Muhammad Idrees <midexigner@gmail.com>
2022-03-29 01:12:21 +0500

    Rest Api for goals CRUD routes

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const {getGoals,setGoal,updateGoal,deleteGoal} = require("../controllers/goalControllers");
 
 router.route('/').get(getGoals).post(setGoal);
-
 router.route('/:id').delete(deleteGoal).put(updateGoal);
 
 module.exports = router;
