c2725cbd21b75d089311f9e5dc28e1701d066887
David <david.toledo@seoprohub.com>
2023-01-28 04:39:10 +0500

    adding pages register and login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
-// import { ToastContainer } from 'react-toastify'
-// import 'react-toastify/dist/ReactToastify.css'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import Dashboard from './pages/Dashboard'
+import { Login } from './pages/Login'
+import { Register } from './pages/Register'
 
 const App = () => {
+  const notify = () => toast("Wow so easy!");
   return (
     <>
     <Router>  
@@ -13,8 +17,11 @@ const App = () => {
     <Header />
     <Routes>
     <Route path='/' element={<Dashboard />} />
+    <Route path='/login' element={<Login />} />
+    <Route path='/register' element={<Register />} />
     </Routes>
-    {/* <ToastContainer /> */}
+    <button onClick={notify}>Notify!</button>
+    <ToastContainer />
     </div>
     </Router>
     </>
