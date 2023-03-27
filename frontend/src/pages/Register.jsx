44fe6ea55864ec459444b18fbf427aca6922cd06
David <david.toledo@seoprohub.com>
2023-03-27 19:04:36 +0500

    some bug fixed

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react'
-// import { useSelector, useDispatch } from 'react-redux'
-// import { useNavigate } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
 import { FaUser } from 'react-icons/fa'
+import {register,reset} from '../features/auth/authSlice'
 
 export const Register = () => {
   const [formData, setFormData] = useState({
@@ -11,10 +12,11 @@ export const Register = () => {
     password: '',
     password2: '',
   });
-  // const navigate = useNavigate()
-  // const dispatch = useDispatch()
+  const navigate = useNavigate();
+  const dispatch = useDispatch()
   const { name, email, password, password2 } = formData;
   
+  const {user,isLoading,isError,isSuccess,message} = useSelector((state)=> state.auth);
   const onChange = (e) => {
     setFormData((prevState) => ({
       ...prevState,
@@ -23,19 +25,22 @@ export const Register = () => {
   }
   const onSubmit = (e) => {
     e.preventDefault()
-    if (name === "") {
-      toast.error('Name is Required Field')
+    if(name === "" && email === "" && password === ""){
+      toast.error('All Field is Required');
+    }
+    else if (name === "") {
+      toast.error('Name is Required Field');
     }else if (email === "") {
-      toast.error('Email is Required Field')
+      toast.error('Email is Required Field');
     }else if (password !== password2) {
-      toast.error('Passwords do not match')
+      toast.error('Passwords do not match');
     } else {
-      // const userData = {
-      //   name,
-      //   email,
-      //   password,
-      // }
-
+      const userData = {
+        name,
+        email,
+        password,
+      }
+console.log(userData);
       
     }
   }
